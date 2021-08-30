3fd0834b63322b03a8ffce76e1bd06d138610d1c
wzh <3471188738@qq.com>
2021-08-30 22:40:40 +0800

    给微服务coupon和member配置了nacos注册中心，再commom中导入相关的依赖

--- gulimall-member/src/main/java/com/wzh/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/wzh/gulimall/member/GulimallMemberApplication.java
@@ -3,9 +3,11 @@ package com.wzh.gulimall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GulimallMemberApplication {
 
     public static void main(String[] args) {
